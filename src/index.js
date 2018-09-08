d960cb71ad0ca65db43865f6d433bece29974515
Tezika <zfk565614653@outlook.com>
2018-09-08 10:32:10 -0600

    add the local tiled source file

--- src/index.js
+++ src/index.js
@@ -15,14 +15,26 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-
+var titlesetName = 'snowWIP';
 
 function preload()
 {
+     //load the tile
+     this.load.tilemapTiledJSON('map', 'assets/tilemap/test.json');
+     this.load.image('tiles','assets/tilemap/snowWIP.png');
 }
 
 function create()
 {
+    var map = this.make.tilemap({ key: 'map' });
+
+    // The first parameter is the name of the tileset in Tiled and the second parameter is the key
+    // of the tileset image used when loading the file in preload.
+    var tiles = map.addTilesetImage(titlesetName, 'tiles');
+
+    // You can load a layer from the map using the layer name from Tiled, or by using the layer
+    // index (0 in this case).
+    var layer = map.createStaticLayer('background', tiles, 0, 0);
 }
 
 function update()
